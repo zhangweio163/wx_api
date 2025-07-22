8f6cd3d91b49132ef9eb2a7f40159a776c30d651
张巍 <1030268572@qq.com>
2025-07-22 15:20:29 +0800

    cookies变量置空

--- conf/setting.py
+++ conf/setting.py
@@ -14,22 +14,6 @@ constants = {
     "session_prefix" : 'wx_session:',
 	"version" : version,
 }
-cookies = {
-		"ptcz": "8565ca6fc09f207978e510b8cbf9abfc5fb51dde89d20c92239d29f086d9c9b5",
-		"RK": "FBV5amd7SO",
-		"ww_lang": "cn,zh",
-		"wwrtx.c_gdpr": "0",
-		"wwrtx.cs_ind": "",
-		"wwrtx.i18n_lan": "zh",
-		"wwrtx.logined": "true",
-		"wwrtx.ltype": "1",
-		"wwrtx.ref": "direct",
-		"wwrtx.refid": "39162987162324445",
-		"wwrtx.sid": "-b_NZVN5Jbsxp4-EU6qD1oa3cy5gOBjk_Ou5CRU7JDYw6F7IrIZcv0OYU5ewyvte-Uw33o-1zc-SE49x-VSHvw",
-		"wwrtx.vid": "1688855053723800",
-		"wwrtx.vst": "GbQdQsjx66DT5FYWHz5w2FXkpVzBvU--H3aJC3Qmr7p8rSfTxG4OFGm_pXPlIHRB4PKuMbIin2x5gy9bLwqd62uf1om7qvdA4_gqO_0W-Fi1vVCMKk2jegazZCf2XWv0_FgcD74dUy3L7VwBQrB46fyJZrn8egIPYUibepAiU5gdxSUFC9WT5ETcijvHPYhkZrrk8_HWrOowxP6F0UhcntF-NS_zhFFaTHVJY3s2hKOGF5qgttByAe7FImBbrXNE7d0ONn1kVUia0Vhtn0IDYFO7F5BwdLKiw3gCEBgNrMY",
-		"wxpay.corpid": "1970326604993422",
-		"wxpay.vid": "1688855053723800"
-	}
+cookies = {}
 
 
